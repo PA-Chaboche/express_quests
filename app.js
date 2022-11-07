2f0e0766f5b37efefa556acd67aa9a1d9fd7efd9
Chaboche Pierre-Alexis <pierrealexischaboche@gmail.com>
2022-11-07 13:15:58 +0100

    express 2

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
+
+const port = process.env.APP_PORT ?? 8000;
+
 const express = require("express");
 
 const app = express();
-
-const port = 5000;
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -11,10 +13,12 @@ const welcome = (req, res) => {
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
+const { getUsers, getUsersById } = require("./usersHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-
+app.get("/api/users", getUsers);
+app.get("/api/users/:id", getUsersById);
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
